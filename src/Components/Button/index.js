f5d723755afa8d8dc76b678c92e116e5181f825e
Luna Yu <yuanluna.yu@hotmail.it>
2022-01-14 14:16:05 +0000

    toggle of value to fetch api at every re-render of the button

--- src/Components/Button/index.js
+++ src/Components/Button/index.js
@@ -2,30 +2,40 @@ import React, {useState, useEffect} from "react";
 
 function Button(){
 
-  const [id, setId] = useState("")
+  const [toggle, setToggle] = useState(false)
 
   useEffect(()=>{
     async function findFoxImage() {
-const promise = await fetch (`https://randomfox.ca//images//${id}.jpg`);
-const data = await promise.json();
-setId(data)
-console.log(data)
-//let image = document.createElement("img")
+    const promise = await fetch (`https://randomfox.ca/floof/`);
+    const data = await promise.json();
+    
+    console.log(data)
+    //let image = document.createElement("img")
     }
     findFoxImage()
-  }
-  )
+  }, [toggle]
+  );
 
   function handleClick() {
-    //set id to be a random number between 1 - 123
-    const ranNum = Math.floor(Math.random() * 123 )
-    console.log(ranNum);
-    setId(ranNum)
+    if(toggle === false){
+      setToggle(true)
+    } else if(toggle === true){
+      setToggle(false)
+    }
+    console.log('working')
   }
+  
+  // function handleClick() {
+  //   // //set id to be a random number between 1 - 123
+
+  //   // const ranNum = Math.ceil(Math.random(0-1) * 123 )
+  //   // console.log(ranNum);
+  //   // setId(ranNum)
+  // } {toggle? 'Foxie' : 'Foxie'}
 
   return (
    <>
-<button onClick = {handleClick}>{id}</button>
+<button onClick = {handleClick}>Click</button>
   </>
   )
 }
