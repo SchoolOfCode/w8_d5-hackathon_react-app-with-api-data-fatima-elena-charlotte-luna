29af13e889db45573ef40c2f1e8215d4112d3fe6
fatima-hashi <fmhashi91@gmail.com>
2022-01-14 12:28:22 +0000

    set id done

--- src/Components/Button/index.js
+++ src/Components/Button/index.js
@@ -1,16 +1,31 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 
 function Button(){
+
+  const [id, setId] = useState("")
+
+  useEffect(()=>{
+    async function findFoxImage() {
+const promise = await fetch (`https://randomfox.ca//images//${id}.jpg`);
+const data = await promise.json();
+setId(data)
+console.log(data)
+nlet image = document.createElement("img")
+    }
+    findFoxImage()
+  }
+  )
 
   function handleClick() {
     //set id to be a random number between 1 - 123
     const ranNum = Math.floor(Math.random() * 123 )
     console.log(ranNum);
+    setId(ranNum)
   }
 
   return (
    <>
-<button onClick = {handleClick}>Click</button>
+<button onClick = {handleClick}>{id}</button>
   </>
   )
 }
