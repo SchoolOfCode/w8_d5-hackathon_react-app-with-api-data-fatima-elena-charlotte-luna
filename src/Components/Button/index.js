1922dbaed5173cf06a8213070992bbf72a1f8479
tinytechernode <tinytecher@gmail.com>
2022-01-14 11:28:30 +0000

    Added Button function

--- src/Components/Button/index.js
+++ src/Components/Button/index.js
@@ -2,13 +2,17 @@ import React from "react";
 
 function Button(){
 
+  function handleClick() {
+    //set id to be a random number between 1 - 123
+    const ranNum = Math.floor(Math.random() * 123 )
+    console.log(ranNum);
+  }
+
   return (
-
    <>
-<button>Click</button>
-
-    </>)
-
+<button onClick = {handleClick}>Click</button>
+  </>
+  )
 }
 
 export default Button
